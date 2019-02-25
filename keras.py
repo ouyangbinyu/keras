3324c635abd88af657afe8e36d0fddf03b3214cd
ouyangbinyu <459972668@qq.com>
2019-02-25 21:25:41 +0800

    加入validation

--- keras.py
+++ keras.py
@@ -17,4 +17,7 @@ model.compile(optimizer = tf.train.AdamOptimizer(0.01),loss = tf.keras.losses.ca
 data = np.random.random((1000,32))
 labels = np.random.random((1000,10))
 
-model.fit(data,labels,epochs=10,batch_size=32)
+val_data = np.random.random((100, 32))
+val_labels = np.random.random((100, 10))
+
+model.fit(data,labels,epochs=10,batch_size=32,validation_data=(val_data, val_labels))
